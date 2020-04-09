2889badfb36c444650d37341dd62334349f0a767
Tony Deng <wolf.deng@gmail.com>
2020-04-09 18:10:34 +0800

    :bug: fix(2.0): 解决build时的commitId没有获取的问题
    获取CommitID的命令没有问题，但是后续再往程序中赋值的变量名称写错了
    
    refs #4
    
    Signed-off-by: Tony Deng <wolf.deng@gmail.com>

--- cmd/root.go
+++ cmd/root.go
@@ -1,9 +1,11 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"github.com/spf13/cobra"
+	"github.com/tonydeng/git-toolkit/utils"
+)
 
 var cfgFile string
-var Verbose bool
 var RootCmd = &cobra.Command{
 	Use:   "git-toolkit",
 	Short: "Git工具集",
@@ -22,7 +24,8 @@ func init() {
 	uninstallCmd.PersistentFlags().StringVar(&installDir, "dir", "/usr/local/bin/", "install dir")
 
 	//持久标志
-	RootCmd.PersistentFlags().BoolVarP(&Verbose, "verbose", "v", false, "verbose out")
+	version := utils.GenVersion(Version, BuildTime, CommitID)
+	RootCmd.SetVersionTemplate(version)
 
 	// 添加自定义Git命令
 	// 安装 & 卸载
