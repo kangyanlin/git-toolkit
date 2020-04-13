9c3fcad590c98d977be0fe9644c9a1fcf65281f0
Tony Deng <wolf.deng@gmail.com>
2020-04-13 16:21:22 +0800

    :sparkles: feat(2.0): 完善制品下载和安装脚本逻辑
    完善制品下载和安装脚本逻辑
    
    refs #4
    
    Signed-off-by: Tony Deng <wolf.deng@gmail.com>

--- installer.sh
+++ installer.sh
@@ -32,17 +32,27 @@ function init() {
 # 下载并重命名
 function download() {
     DOWNLOAD_URL="$REPO_HOME/raw/$BRANCH/dist/$PRODUCTION"
-    echo "download  $DOWNLOAD_URL"
-    if command -v wget >/dev/null 2>&1; then
-        echo "wget $DOWNLOAD_URL"
-    elif command -v curl >/dev/null 2>&1; then
-        echo "curl -fsSL $DOWNLOAD_URL"
+    echo "################### download  $DOWNLOAD_URL ###################"
+
+    if [[ ! -z "$PRODUCTION" ]] ; then
+        rm "./$PRODUCTION" # 删除当前目录已经下载的安装包    
+        if command -v wget >/dev/null 2>&1; then
+            echo "wget -c -t 0  $DOWNLOAD_URL"
+            wget -c -t 0 "$DOWNLOAD_URL"
+        elif command -v curl >/dev/null 2>&1; then
+            echo "curl -fsSL -C - -O $DOWNLOAD_URL"
+            curl -fsSL -C - -O "$DOWNLOAD_URL"
+        fi
     fi
 }
 
 # 安装
 function install() {
-    echo "install"
+    echo ""
+    echo "################### install $PRODUCTION ###################"
+    if [[ ! -z "$PRODUCTION" && -f "./$PRODUCTION" ]] ; then
+        "./$PRODUCTION install"
+    fi
 }
 
 init
